4636969f81c19bcc14b8217209017b4e4c136cf6
litao <ltipchrome@gmail.com>
2018-08-13 22:25:36 +0800

    wip

--- constrained.py
+++ constrained.py
@@ -2,98 +2,156 @@
 from unconstrained import line_search
 from numpy.linalg import inv, solve, norm
 import numpy as np
+import copy
 
 
-def newton_step(grad, hessian, A, x, b, feasible=True):
-    """
-    solve the KKT system
+class EqualityCvx:
+    def __init__(self, f0, grad_f, hessian_f, A, b, x0, epsilon=1e-5, alpha=.3, beta=.8):
+        self.f0 = f0
+        self.A = A
+        self.b = b
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.beta = beta
+        self.x0 = x0
+        self.grad = grad_f
+        self.hessian = hessian_f
+        self._hessian = hessian_f(x0)
+        self._grad = grad_f(x0)
+        self.x = x0
+        self._decrement = None
+        self.optimal = None
+        self.step = None
+        self.w = None
+        self.v = x0   # next dual variable can be calculated directly by v_plus = w
+        self.v_plus = self.w
+        self.dual_step = None
 
-    :param grad:
-    :param hessian:
-    :param A:
-    :return:
-    """
+    def newton_step(self, feasible=True):
+        """
+        solve the KKT system
+        """
+        x = self.x
+        _hessian = self._hessian = self.hessian(x)
+        _grad = self._grad = self.grad(x)
+        A, b= self.A, self.b
+        n, p = A.shape[0]
 
-    first_col = np.vstack([hessian, A])
-    n, p = A.shape[0]
-    _zero = np.zeros((n, n))
-    second_col = np.vstack([A.T, _zero])
-    KKT_matrix = np.hstack([first_col, second_col])
-    if feasible:
-        RHS = np.concatenate((-grad, np.zeros(n)))[:, np.newaxis]
-    else:
-        RHS = np.concatenate((-grad, A@x - b))[:, np.newaxis]
-    sol = solve(KKT_matrix, RHS)
-    step = sol.flatten()[:n]
-    w = sol.flatten()[n:]
-    return step, w
+        first_col = np.vstack([_hessian, A])
+        _zero = np.zeros((n, n))
+        second_col = np.vstack([A.T, _zero])
+        KKT_matrix = np.hstack([first_col, second_col])
 
+        if feasible:
+            RHS = np.concatenate((-_grad, np.zeros(n)))[:, np.newaxis]
+        else:
+            RHS = np.concatenate((-_grad, A @ x - b))[:, np.newaxis]
+        sol = solve(KKT_matrix, RHS)
+        step = sol.flatten()[:n]
+        w = sol.flatten()[n:]
+        self.step = step
+        self.w = w
+        self.v_plus = w
+        self.dual_step = w - self.v
+        return step
 
-def newton_decrement(grad, hessian):
-    return (grad.T@inv(hessian)@grad)**(1/2)
+    @property
+    def newton_decrement(self):
+        grad, hessian = self._grad, self._hessian
+        self._decrement = (grad.T@inv(hessian)@grad)**(1/2)
+        return self._decrement
 
+    def newton_method(self):
+        while True:
+            epsilon, f0, alpha, beta = self.epsilon, self.f0, self.alpha, self.beta
+            step, w = self.newton_step(feasible=True)
+            decrement = self.newton_decrement
+            if 1/2 * decrement**2 < epsilon:
+                break
+            t = line_search(step, f0, self._grad, alpha=alpha, beta=beta)
+            self.x += t*step
+        self.optimal = self.x
 
-def newton_method(f, grad, hessian, A, x0, epsilon=1e-5, alpha=.3, beta=.8):
-    x = x0
-    while True:
-        _grad = grad(x)
-        _hessian = hessian(x)
-        step, w = newton_step(_grad, _hessian, A)
-        decrement = newton_decrement(_grad, _hessian)
-        if 1/2 * decrement**2 < epsilon:
-            break
-        t = line_search(step, f, _grad, alpha=alpha, beta=beta)
-        x += t*step
-    return x
+    def infeasible_newton(self):
+        epsilon, alpha, beta, A, b = self.epsilon, self.alpha, self.beta, self.A, self.b
+        while True:
+            _ = self.newton_step(feasible=False)
+            t = self.infeasible_line_search()
+            self.x += t*self.step
+            self.v += t*self.dual_step
+            if np.isclose(A@self.x, b) and (self.residual(self.x, self.v) <= epsilon):
+                break
+        self.optimal = self.x
 
+    def residual(self, x, v):
+        A, b, _grad = self.A, self.b, self._grad
+        r = np.vstack([_grad + A.T@v, A@x - b])
+        return r
 
-# infeasible start
+    def infeasible_line_search(self):
+        t = 1
+        alpha, beta, x, v = self.alpha, self.beta, self.x, self.v
+        norm_r = norm(self.residual(x, v))
+        while True:
+            nr = self.residual(x + t*self.step, v + t*self.dual_step)
+            if norm(nr) < (1-alpha*t)*norm_r:
+                break
+            t = beta * t
+        return t
 
-def infeasible_newton(grad, hessain, A, x0, b, epsilon=1e-5, alpha=.3, beta=.8):
-    x = x0
-    while True:
-        step, v = newton_step(grad, hessain, A, x, b, feasible=False)
-        t = infeasible_line_search(x, v, step, v, alpha=alpha, beta=beta)
-        x += t
-        if np.isclose(A@x, b) and (residual(x, v) <= epsilon):
-            break
-    return x
+    def minimize(self, feasible=True):
+        if feasible:
+            self.newton_method()
+        else:
+            self.infeasible_newton()
+        return self.optimal
 
-
-def infeasible_line_search(x, v, primal_step, dual_step, alpha=.3, beta=.8):
-    t = 1
-    norm_r = lambda x, y: norm(residual(x, y))
-    while norm_r(primal_step, dual_step) > (1-alpha*t)*norm_r(x, v):
-        t = beta * t
-    return t
-
-
-def residual(x, y):
-    pass
-
-
-# interior-point method
+# ------------------------------ interior-point method -----------------------
 # x*(t) is m/t-suboptimal
+
+
 def log_barrier(u, t):
     return -(1/t)*np.log(u)
 
 
-def barrier_method(x0, f, gradt, hessiant, A, fi, epsilon=1e-5, alpha=.3, beta=.8, u=20):
-    # centering step
-    x = x0
-    t = choose_t()
-    m = len(fi)
-    while True:
-        _f = lambda x: t*(f(x) + sum(log_barrier(_fi(x), t) for _fi in fi))
-        x = newton_method(_f, gradt(t), hessiant(t), A, x, epsilon=epsilon, alpha=alpha, beta=beta, u=u)
-        if m/t < epsilon:
-            break
-        t *= u
-    return x
+class IneCvx(EqualityCvx):
 
+    def __init__(self, f0, fis, grad_f, hessian_f, A, b, x0, epsilon=1e-5, alpha=.3, beta=.8, u=20):
+        super().__init__(f0, grad_f, hessian_f, A, b, x0, epsilon=epsilon, alpha=alpha, beta=beta)
+        self.fis = fis
+        self.u = u
+        self.ecvx = self.copy()
 
-def choose_t():
-    return NotImplementedError
+    def copy(self):
+        return copy.deepcopy(self)
+
+    def barrier_method(self):
+        # centering step
+        t = self.choose_t()
+        fis = self.fis
+        m = len(fis)
+        while True:
+            # outer loop
+            self.ecvx = self.copy()
+            self.ecvx.f0 = lambda x: t * (self.f0(x) + sum(log_barrier(_fi(x), t) for _fi in fis))
+            self.ecvx.grad = self.grad_ft(t)
+            self.ecvx.hessian = self.hessian_ft(t)
+            self.ecvx.x0 = self.x
+            self.x = self.ecvx.minimize()
+            if m/t < self.epsilon:
+                break
+            t *= self.u
+        self.optimal = self.x
+        return self.x
+
+    def choose_t(self):
+        return NotImplementedError
+
+    def grad_ft(self, t):
+        return NotImplementedError
+
+    def hessian_ft(self, t):
+        return NotImplementedError
 
 
 
